ca59d5f648a645482faf8e908c0199c7eada913d
vzohni <vzohni@gmail.com>
2019-06-14 12:08:10 -0400

    Uploaded Material-Ui client & changed names
    Created a react app using "create-react-app" template, then added material-ui and used their sign in page tempalte. Changed said templates to fit our project.
    
    Replace this client with the facealbun folder, and changed the names to make Shums happy

--- spotifysocial-client/README.md
+++ spotifysocial-client/README.md
@@ -1,3 +1,15 @@
+TEAM SIAKAM
+
+Spotify Social - Client
+
+Made with create-react-app and material UI
+
+Victor
+Pratik
+Juan
+
+---
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
---
+++ spotifysocial-client/src/App.js
@@ -0,0 +1,75 @@
+
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import LibraryMusic from '@material-ui/icons/LibraryMusic';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(https://upload.wikimedia.org/wikipedia/commons/e/ec/Record-Album-02.jpg)',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LibraryMusic />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Spotify Social
+          </Typography>
+          <form className={classes.form} noValidate>
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="secondary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+
+          </form>
+        </div>
+      </Grid>
+    </Grid>
+  );
+}
