a173766b4b4003d7428641839a46a973802f55fe
vzohni <vzohni@gmail.com>
2019-06-26 19:45:07 -0400

    Added front end functionality to change user description
    Uses Pratik's patch code to change the back end. Once the user hits enter, the pages refreshes, and description is updated.

--- tuneful-server/src/users/users-router.js
+++ tuneful-server/src/users/users-router.js
@@ -1,22 +1,40 @@
-const express = require('express')
 const path = require('path')
+const express = require('express')
+const xss = require('xss')
 const UsersService = require('./users-service')
 
 const usersRouter = express.Router()
 const jsonBodyParser = express.json()
 
-usersRouter
-  .post('/', jsonBodyParser, (req, res, next) => {
-    const { first_name, last_name, email,password } = req.body
+const serializeUser = user => ({
+  id: user.id,
+  first_name: xss(user.first_name),
+  last_name: xss(user.last_name),
+  email: xss(user.email),
+  description: xss(user.description),
+  date_created: user.date_created,
+})
 
-    for (const field of ['first_name', 'last_name','email', 'password'])
+usersRouter
+  .route('/')
+  .get((req, res, next) => {
+    const knexInstance = req.app.get('db')
+    UsersService.getAllUsers(knexInstance)
+      .then(users => {
+        res.json(users.map(serializeUser))
+      })
+      .catch(next)
+  })
+  .post(jsonBodyParser, (req, res, next) => {
+    const { first_name, last_name, password, email } = req.body
+
+    for (const field of ['first_name', 'last_name', 'email', 'password'])
       if (!req.body[field])
         return res.status(400).json({
           error: `Missing '${field}' in request body`
         })
 
     const passwordError = UsersService.validatePassword(password)
-
     if (passwordError)
       return res.status(400).json({ error: passwordError })
 
@@ -26,15 +44,15 @@ usersRouter
     )
       .then(hasUserWithEmail => {
         if (hasUserWithEmail)
-          return res.status(400).json({ error: `Email already taken` })
+          return res.status(400).json({ error: `Username already taken` })
 
         return UsersService.hashPassword(password)
           .then(hashedPassword => {
             const newUser = {
               first_name,
               last_name,
-              email,
               password: hashedPassword,
+              email,
               date_created: 'now()',
             }
 
@@ -46,9 +64,63 @@ usersRouter
                 res
                   .status(201)
                   .location(path.posix.join(req.originalUrl, `/${user.id}`))
-                  .json(UsersService.serializeUser(user))
+                  .json(serializeUser(user))
               })
           })
+      })
+      .catch(next)
+  })
+
+usersRouter
+  .route('/:user_id')
+  .all((req, res, next) => {
+    UsersService.getById(
+      req.app.get('db'),
+      req.params.user_id
+    )
+      .then(user => {
+        if (!user) {
+          return res.status(404).json({
+            error: { message: `User doesn't exist` }
+          })
+        }
+        res.user = user
+        next()
+      })
+      .catch(next)
+  })
+  .get((req, res, next) => {
+    res.json(serializeUser(res.user))
+  })
+  .delete((req, res, next) => {
+    UsersService.deleteUser(
+      req.app.get('db'),
+      req.params.user_id
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
+  .patch(jsonBodyParser, (req, res, next) => {
+    const { first_name, last_name, password, email, description } = req.body
+    const userToUpdate = { first_name, last_name, password, email, description }
+
+    const numberOfValues = Object.values(userToUpdate).filter(Boolean).length
+    if (numberOfValues === 0)
+      return res.status(400).json({
+        error: {
+          message: `Request body must content either 'firstname', 'lastname', 'password' ,'email' or 'description'`
+        }
+      })
+
+    UsersService.updateUser(
+      req.app.get('db'),
+      req.params.user_id,
+      userToUpdate
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
       })
       .catch(next)
   })
